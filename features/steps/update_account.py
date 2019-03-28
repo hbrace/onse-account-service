115c1aeb9c7caab0f7ef519a1d5cccaa04a567a2
hbrace <hmf.brace@gmail.com>
2019-03-28 11:41:57 +0000

    test works now

--- features/steps/update_account.py
+++ features/steps/update_account.py
@@ -6,9 +6,13 @@ from features.steps.get_account import get_account_by_context, \
 CREATE_ACCOUNT_URL = f'/accounts/'
 
 
-@when('I update the status of an account with id "{account_id}" to "{status}"')
-def update_account(context, account_id, status):
-    pass
+@when('I update the status of an account with id "{account_number}" to "{status}"')
+def update_account(context, account_number, status):
+    response = context.web_client.put(
+        f'/accounts/{account_number}',
+        json={'status': status})
+
+    assert response.status_code == 200, response.status_code
 
 
 @then('Account with id "{account_id}" should be "{status}"')
--- features/update_account.feature
+++ features/update_account.feature
@@ -3,7 +3,6 @@ Feature: Update account
   I want to update an account
 
   Scenario: An account is updated successfully
-#        Given Account with ID "1" exists
     Given there an "active" account with customer id "1"
     When I update the status of an account with id "1" to "closed"
     Then Account with id "1" should be "closed"
